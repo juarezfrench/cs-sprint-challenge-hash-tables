34382c0531476d7249ebde140999495d78523daa
juarezfrench <markedfrench@gmail.com>
2020-08-07 12:44:34 -0500

    Completed ex1 - MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    values = {}
+    for i in range(len(weights)):
+        if weights[i] not in values:
+            values[weights[i]] = i
+        co_key = limit - weights[i]
+        if co_key in values and values[co_key] is not i:
+            co_index = values[co_key]
+            if i >= co_index:
+                return [i, co_index]
+            else:
+                return [values[co_key], i]
     return None
+
+  
